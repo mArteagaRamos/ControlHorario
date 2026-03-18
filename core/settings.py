2afc03817162d81e8710cff6f5f38942807e97c8
Maria Arteaga <maria.arteaga@aeptic.es>
2026-03-18 13:05:57 +0100

    actualizo estructura proyecto

--- core/settings.py
+++ core/settings.py
@@ -38,8 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.postgres',
-    'timetracking',
-    'users',
+    'apps.timetracking',
+    'apps.users',
 ]
 
 MIDDLEWARE = [
